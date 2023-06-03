dddb547ff68a6f4129f370739a18c4921ac6af1d
Rishabh Raghav <rishabhraghav2012@gmail.com>
2023-06-03 16:54:10 +0530

    cors policy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://medico-sks.netlify.app',
 }));
 
 app.use((req, res, next) => {
