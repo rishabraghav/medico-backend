001c1e47b05fdd14964aa629166df78520f16826
Rishabh Raghav <rishabhraghav2012@gmail.com>
2023-07-25 23:08:28 +0530

    added new collection for patients

---
+++ Routes/medicineRoutes.js
@@ -0,0 +1,99 @@
+
+const Medicine = require('./medicineModule');
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    // Fetch data from MongoDB
+    // console.log("hey");
+
+    Medicine.find()
+      .then(data => {
+        // Send the retrieved data as a response
+        // console.log("server side get method data ",data);
+        res.json(data);
+      })
+      .catch(error => {
+        console.error('Error retrieving data:', error);
+        res.status(500).send('Internal Server Error');
+        // console.log("server side get method data ",data);
+      });
+  });
+
+  router.get("/search", (req, res) =>  {
+    const searchTerm = req.body.searchTerm;
+    Medicine.find({ name: { $regex: searchTerm, $options: 'i' } })
+    .then(res.json(data))
+    .catch((error) => {
+      console.error('Error searching for medicine:', error);
+      res.status(500).send('Internal Server Error');
+    });
+  });
+
+  router.post('/add', (req, res) => {
+    const {name, description, quantity} = req.body;
+
+    Medicine.findOne({ name })
+    .then(existingMedicine => {
+      if (existingMedicine) {
+        // If the medicine already exists, update its description and quantity
+        existingMedicine.description = description;
+        existingMedicine.quantity = quantity;
+
+        existingMedicine
+          .save()
+          .then(updatedMedicine => {
+            res.json(updatedMedicine);
+          })
+          .catch(error => {
+            console.error('Error updating medicine:', error);
+            res.status(500).send('Internal Server Error');
+          });
+      } else {
+        // If the medicine doesn't exist, create a new one
+        const newMedicine = new Medicine({
+          name: name,
+          description: description,
+          quantity: quantity
+        });
+
+        newMedicine
+          .save()
+          .then(savedMedicine => {
+            res.json(savedMedicine);
+          })
+          .catch(error => {
+            console.error('Error saving medicine:', error);
+            res.status(500).send('Internal Server Error');
+          });
+      }
+    })
+    .catch(error => {
+      console.error('Error finding medicine:', error);
+      res.status(500).send('Internal Server Error');
+    });
+  });
+
+  router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    
+    Medicine.deleteOne({ _id: id })
+  .then(deletedMedicine => {
+    if (deletedMedicine.deletedCount === 0) {
+      // If the medicine with the provided ID doesn't exist
+      console.log("id:", id);
+      return res.status(404).json({ message: 'Medicine not found' });
+    }
+
+    res.json({ message: 'Medicine deleted successfully' });
+  })
+  .catch(error => {
+    console.error('Error deleting medicine:', error);
+    res.status(500).send('Internal Server Error');
+  });
+  });
+
+
+  module.exports = router;
---
+++ Routes/patientRoutes.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const Patient = require('./patientsModel');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  Patient.find()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+});
+
+router.post('/add', (req, res) => {
+  const patient = req.body;
+  // console.log(patient.Info);
+  const InfoArray = patient.Info;
+  const lastElement = InfoArray[InfoArray.length - 1];
+  console.log(lastElement);
+
+  Patient.findOneAndUpdate(
+    { contact: patient.contact },
+    { $push: { Info: lastElement } },
+    { new: true })
+    .then((updatedPatient) => {
+      if (updatedPatient) {
+        console.log("patient updated: ", updatedPatient);
+      } else {
+        const newPatient = new Patient({
+          Uhid: patient.Uhid,
+          name: patient.name,
+          age: patient.age,
+          sex: patient.sex,
+          address: patient.address,
+          contact: patient.contact,
+          Info: InfoArray,
+        });
+
+        newPatient.save()
+          .then((savedPatient) => {
+            console.log('Patient saved:', savedPatient);
+            res.json(savedPatient);
+          })
+          .catch((error) => {
+            console.error('Error saving patient:', error);
+            res.status(500).json({ error: 'Failed to save patient.' });
+          });
+      }
+    })
+    .catch((error) => {
+      console.error("error in updating the patient: ", error);
+    })
+});
+
+
+router.delete('/:id/:infoId', async (req, res) => {
+  const { id, infoId } = req.params;
+  console.log(id, infoId);
+  try {
+    const updatedPatient = await Patient.findByIdAndUpdate(
+      id,
+      { $pull: { Info: { _id: infoId } } },
+      { new: true }
+    );
+
+    if(!updatedPatient) return res.status(404).json({error: "Patient not found in database"}); 
+    return res.json({message: "Patient Info is deleted from the database successfully"});
+  } catch (err) {
+    console.error("error in deleting the patient record from server side: ", err);
+
+    return res.status(500).json({error: "error in deleting patient Info from server side"});
+  }
+  });
+
+
+module.exports = router;
