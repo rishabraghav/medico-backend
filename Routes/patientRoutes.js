d150e22b9f87ef56d14f3df1fea29d3e10111d6f
Rishabh Raghav <rishabhraghav2012@gmail.com>
2023-08-03 23:10:16 +0530

    changed database back to mongoAtlas

--- Routes/patientRoutes.js
+++ Routes/patientRoutes.js
@@ -12,8 +12,7 @@ router.get('/', (req, res) => {
     .catch((error) => {
       cconsole.error('Error retrieving data:', error);
       res.status(500).send('Internal Server Error');
-    })
-    
+    });
 });
 
 router.post('/add', (req, res) => {
